23747ab8f01bc19e728d6751bec791e5e68d97d1
Peter Varga <varga.peter826@gmail.com>
2020-06-23 23:02:39 +0200

    Updates

---
+++ precache-manifest.9a45223ed58f70124caceaddcb12b921.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a72e34c60426b143946b1a5e691b7cb",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "34debb5c6c31ceaae761",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f2e8497ca219d24ba5a6",
+    "url": "/robofriend/static/css/main.9fe30eb5.chunk.css"
+  },
+  {
+    "revision": "34debb5c6c31ceaae761",
+    "url": "/robofriend/static/js/2.a6decf02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.a6decf02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e8497ca219d24ba5a6",
+    "url": "/robofriend/static/js/main.8f1b4261.chunk.js"
+  },
+  {
+    "revision": "7babbf9b1be9d9e8ed65",
+    "url": "/robofriend/static/js/runtime-main.4e601082.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.dc709ee7c15584e5c29eaf3f41a6ec13.js"
+  "/robofriend/precache-manifest.9a45223ed58f70124caceaddcb12b921.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
